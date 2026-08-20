2efabf39274c76a31df9625eab4ef7e548c9a3d8
@jcbalaho31 <jcbalaho31@src.pshs.edu.ph>
2026-08-20 08:57:59 +0800

    Update Improved Distance Calculator Code by Balaho.py

--- Improved Distance Calculator Code by Balaho.py
+++ Improved Distance Calculator Code by Balaho.py
@@ -1,14 +1,14 @@
-# This code calculates distance between 2 points based on the user's inputs.
-# 1st of all, we need to import the math module to use the sqrt and pow functions.
+# This program calculates and finds the distance between 2 points based on the user's inputs.
+# 1st of all, we need to import the math module in order to use the sqrt and pow functions.
 import math
 
-# Asks the user to input the coordinates of 2 points
+# The program then asks the user to input the coordinates of 2 points
 x1 = int(input("Enter the x-coordinate of the first point: "))
 y1 = int(input("Enter the y-coordinate of the first point: "))
 x2 = int(input("Enter the x-coordinate of the second point: "))
 y2 = int(input("Enter the y-coordinate of the second point: "))
 
-# Calculates the distance between the 2 points and then prints the result
+# It then calculates the distance between the 2 points and then prints the result
 distance = math.sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2))
 
 print("The distance between the 2 points is", distance)
